a508aa17e8f677d379d28998fdc5a61277dcbc1e
Knar33 <adbyerly@comcast.net>
2018-05-09 23:01:40 -0500

    Messing with cookies

--- PhoenixRising.Website/Controllers/HomeController.cs
+++ PhoenixRising.Website/Controllers/HomeController.cs
@@ -10,19 +10,20 @@ namespace PhoenixRising.Website.Controllers
     {
         public ActionResult Index()
         {
+            Response.Cookies.Add(new HttpCookie("AuthToken")
+            {
+                Value = "123456",
+                HttpOnly = true,
+                Expires = DateTime.Now.AddMinutes(1)
+            });
+            
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            HttpCookie cookie = Request.Cookies.Get("AuthToken");
+            ViewBag.Message = cookie.Value;
 
             return View();
         }
