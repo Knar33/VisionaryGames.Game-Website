6685cc4b126e8fa12644d585d33188a9a0d064f0
Knar33 <adbyerly@comcast.net>
2018-05-14 22:56:39 -0500

    Build register page

---
+++ PhoenixRising.Website/Controllers/AccountController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using PhoenixRising.Website.Models;
+using PhoenixRising.InternalAPI.Website;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using PhoenixRising.InternalAPI.Authentication;
+using PhoenixRising.InternalAPI;
+
+namespace PhoenixRising.Website.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            HttpCookie username = Request.Cookies.Get("UserID");
+            if (username != null)
+            {
+                ViewBag.Username = username.Value;
+            }
+
+            return View();
+        }
+        
+        // GET: Account
+        public ActionResult PasswordReset()
+        {
+            return View();
+        }
+
+        // POST: Account
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PasswordReset(PasswordReset model)
+        {
+            return View();
+        }
+
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Create user request
+                APIConnection connection = new APIConnection(ConfigurationManager.AppSettings["InternalAPIURL"]);
+                KeyVaultClient KeyVault;
+                try
+                {
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var _token = azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net").Result;
+                    KeyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                var bundle = KeyVault.GetSecretAsync(ConfigurationManager.AppSettings["AzureVaultURL"]).Result;
+                
+                CreateUserRequest request = new CreateUserRequest(connection);
+                request.Email = model.Email;
+                request.FirstName = model.FirstName;
+                request.LastName = model.LastName;
+                request.Nicknane = model.Nicknane;
+                request.Password = model.password1;
+                request.AppAccessToken = bundle.Value;
+
+                CreateUserResponse response = request.Send();
+                
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    //make login request
+                    LoginRequest loginRequest = new LoginRequest(connection, model.Email, model.password1);
+                    LoginResponse loginResponse = loginRequest.Send();
+
+                    if (loginResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        //Create login cookies
+                        Response.Cookies.Add(new HttpCookie("AccessToken")
+                        {
+                            Value = loginResponse.access_token,
+                            HttpOnly = true
+                        });
+                        Response.Cookies.Add(new HttpCookie("UserID")
+                        {
+                            Value = loginResponse.user_id,
+                            HttpOnly = true
+                        });
+                        Response.Cookies.Add(new HttpCookie("AccessTokenExpiration")
+                        {
+                            Value = loginResponse.expireTime,
+                            HttpOnly = true
+                        });
+                        Response.Cookies.Add(new HttpCookie("RefreshToken")
+                        {
+                            Value = loginResponse.refresh_token,
+                            HttpOnly = true
+                        });
+
+                        //redirect to register success, login success
+                        TempData["Success"] = "You have successfully registered an account, and have automatically been signed in.";
+                        return RedirectToAction("Index", "Account");
+                    }
+                    else
+                    {
+                        TempData["Success"] = "You have successfully registered an account.";
+                        return RedirectToAction("Index", "Account");
+                    }
+                }
+                else
+                {
+                    //add tempdata errors
+                    if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        TempData["Errors"] = "Bad request";
+                    }
+                    else
+                    {
+                        TempData["Errors"] = "There was an error processing your request. Please try again.";
+                    }
+
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login model)
+        {
+            return View();
+        }
+    }
+}
---
+++ PhoenixRising.Website/Models/Register.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRising.Website.Models
+{
+    public class Register
+    {
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Username")]
+        public string Nicknane { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string password1 { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [System.ComponentModel.DataAnnotations.Compare("password1")]
+        public string password2 { get; set; }
+    }
+}
