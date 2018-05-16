f5a0890bcce4769c0f291ac13be694bbdfee75db
Knar <knarxr@protonmail.com>
2018-05-15 23:17:12 -0500

    Added Password action. Added Refresh to login-required pages if token expired

---
+++ PhoenixRising.Website/Models/Password.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRising.Website.Models
+{
+    public class Password
+    {
+        public string token { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [Display(Name = "New Password")]
+        public string password { get; set; }
+    }
+}
