fd49f323a53fd8b25504dc96b730f05dd77f6b81
Knar <knarxr@protonmail.com>
2018-05-16 20:20:11 -0500

    Modified layout

--- PhoenixRising.Website/Controllers/HomeController.cs
+++ PhoenixRising.Website/Controllers/HomeController.cs
@@ -11,20 +11,6 @@ namespace PhoenixRising.Website.Controllers
     {
         public ActionResult Index()
         {
-            Response.Cookies.Add(new HttpCookie("AuthToken")
-            {
-                Value = "123456",
-                HttpOnly = true,
-                Expires = DateTime.Now.AddMinutes(1)
-            });
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            HttpCookie cookie = Request.Cookies.Get("AuthToken");
-            ViewBag.Message = cookie.Value;
-
             return View();
         }
     }
