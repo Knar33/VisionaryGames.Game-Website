faa0397485a017d271269e70e106339709df1d1a
Knar33 <adbyerly@comcast.net>
2018-05-20 18:17:05 -0500

    Added Resend Email (not fully implemented yet)

---
+++ PhoenixRising.Website/Models/Resend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRising.Website.Models
+{
+    public class Resend
+    {
+        [Required]
+        public string EmailResend { get; set; }
+    }
+}
