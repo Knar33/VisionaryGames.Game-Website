84e7ec05cacae46be921134ea7f08899dd1f2c63
Knar33 <adbyerly@comcast.net>
2018-05-20 17:22:44 -0500

    Added actions for editing info, changing email and changing password

---
+++ PhoenixRising.Website/Models/EditInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRising.Website.Models
+{
+    public class EditInfo
+    {
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Username")]
+        public string Nicknane { get; set; }
+    }
+}
