2d8983d87cc90c041793dd639a4f339be1e21443
Knar33 <adbyerly@comcast.net>
2018-05-20 20:17:40 -0500

    Added Owin cookie auth

---
+++ PhoenixRising.Website/App_Start/Startup.Auth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace WebApplication4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+        
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                CookieSecure = CookieSecureOption.Always,
+                ExpireTimeSpan = TimeSpan.FromDays(30)
+            });
+        }
+    }
+}
