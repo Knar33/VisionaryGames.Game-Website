1e7b797ef8fd1d9dab5c64dac557a8dc7d78214c
Knar <knarxr@protonmail.com>
2018-05-21 22:49:21 -0500

    Updated how Refresh works

---
+++ PhoenixRising.Website/Models/UpdatePermissions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoenixRising.Website.Models
+{
+    public class UpdatePermissions
+    {
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        public bool Administrator { get; set; }
+        public bool Developer { get; set; }
+        public bool Banned { get; set; }
+    }
+}
