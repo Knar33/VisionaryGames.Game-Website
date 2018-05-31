0a54deaf907afc99500855afd8ba96f496c95a3c
Knar <knarxr@protonmail.com>
2018-05-31 01:57:58 -0500

    Created download controller. Added server download link

---
+++ PhoenixRising.Website/Controllers/DownloadController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using PhoenixRising.Website.Models;
+using PhoenixRising.InternalAPI.App.DownloadURL;
+using PhoenixRising.Website.Filters;
+using Microsoft.Owin.Security;
+
+namespace PhoenixRising.Website.Controllers
+{
+    public class DownloadController : Controller
+    {
+        //Game download
+        [CookieAuthentication]
+        [AuthorizeUser(Roles = "Developer")]
+        public ActionResult GameClient(PasswordReset model)
+        {
+            string connection = ConfigurationManager.AppSettings["InternalAPIURL"];
+            var appAccessToken = WebUtils.GetVaultSecret("AppConnectionKey");
+
+            DownloadClientRequest downloadGameRequest = new DownloadClientRequest(connection, appAccessToken);
+            DownloadClientResponse downloadGameResponse = downloadGameRequest.Send();
+
+            if (downloadGameResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Redirect(downloadGameResponse.Content);
+            }
+            else
+            {
+                TempData["Errors"] = "There was an error processing your request";
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        //Game download
+        [CookieAuthentication]
+        [AuthorizeUser(Roles = "Developer")]
+        public ActionResult GameServer(PasswordReset model)
+        {
+            string connection = ConfigurationManager.AppSettings["InternalAPIURL"];
+            var appAccessToken = WebUtils.GetVaultSecret("AppConnectionKey");
+
+            DownloadServerRequest downloadServerRequest = new DownloadServerRequest(connection, appAccessToken);
+            DownloadServerResponse downloadServerResponse = downloadServerRequest.Send();
+
+            if (downloadServerResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Redirect(downloadServerResponse.Content);
+            }
+            else
+            {
+                TempData["Errors"] = "There was an error processing your request";
+                return RedirectToAction("Index", "Account");
+            }
+        }
+    }
+}
