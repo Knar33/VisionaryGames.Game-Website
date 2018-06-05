77a6c40b9325203025a5baa5c8debcea84d09b9d
Knar33 <adbyerly@comcast.net>
2018-06-04 22:24:29 -0500

    Created homepage view

--- PhoenixRising.Website/Controllers/HomeController.cs
+++ PhoenixRising.Website/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace PhoenixRising.Website.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index", "~/Views/Shared/_Homepage.cshtml");
         }
     }
 }
